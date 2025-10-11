873b718fc06f7b07e97007d15b7d239620753de9
ratul <ratulbmg@gmail.com>
2025-10-12 00:24:25 +0530

    fix header dark/light mode css

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,11 +1,6 @@
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import type { AppDispatch, RootState } from '../../redux/store';
-import { SiSocialblade } from 'react-icons/si';
-import { MdOutlineClearAll } from 'react-icons/md';
-import { RxVideo } from 'react-icons/rx';
-import { HiDocumentText } from 'react-icons/hi2';
-import { IoLinkSharp } from 'react-icons/io5';
 import { CiMenuKebab } from "react-icons/ci";
 import Modal from '../ui/Modal';
 import { useState } from 'react';
@@ -23,11 +18,11 @@ const Header: React.FC = () => {
     const dispatch = useDispatch<AppDispatch>()
     const status = useSelector((state: RootState) => state.auth.status)
     const navItems = [
-        { name: "All Collection", path: "/dashboard/all", active: true, icon: <MdOutlineClearAll fontSize="20px" /> },
-        { name: "Social Post", path: "/dashboard/twitter", active: true, icon: <SiSocialblade fontSize="20px" /> },
-        { name: "Videos", path: "/dashboard/youtube", active: true, icon: <RxVideo fontSize="20px" /> },
-        { name: "Docs", path: "/dashboard/docs", active: true, icon: <HiDocumentText fontSize="20px" /> },
-        { name: "Links", path: "/dashboard/links", active: true, icon: <IoLinkSharp fontSize="20px" /> }
+        { name: "All Collection", path: "/dashboard/all", active: true },
+        { name: "Social Post", path: "/dashboard/twitter", active: true},
+        { name: "Videos", path: "/dashboard/youtube", active: true},
+        { name: "Docs", path: "/dashboard/docs", active: true},
+        { name: "Links", path: "/dashboard/links", active: true}
     ]
     const handelLogout = () => {
         dispatch(logoutUser())
@@ -36,21 +31,20 @@ const Header: React.FC = () => {
 
     return (
         <>
-            <div className={cn('fixed w-full h-16 flex justify-between items-center bg-white dark:bg-gray-800 px-10 z-50')} >
+            <div className={cn('fixed w-full h-16 flex justify-between items-center bg-header-background dark:bg-header-background-dark px-10 z-50')} >
                 {status ? (
                     <>
                         <div className={cn('lg:hidden')}>
-                            <button onClick={() => setIsOpen(!isOpen)}> <CiMenuKebab fontSize="35px" /> </button>
+                            <button onClick={() => setIsOpen(!isOpen)}> <CiMenuKebab fontSize="35px" className={cn('text-text-300 dark:text-text-dark-100 cursor-pointer')}/> </button>
                         </div>
                         <div id="logo" className={cn('hidden lg:block')}>
-                            <h1 onClick={() => navigate('/')} className={cn(' px-4 py-1 rounded-full font-bold text-white bg-black')}>S-BRAIN</h1>
+                            <h1 onClick={() => navigate('/')} className={cn('text-text-300 dark:text-text-dark-100 font-bold tracking-widest')}>S-BRAIN</h1>
                         </div>
                         <div id="nav-items" className={cn('hidden lg:block')}>
                             {status && (
-                                <div className={cn('m-auto max-w-[640px] w-full h-[45px] bg-white rounded-full flex justify-between items-center px-[6px]')}>
+                                <div className={cn('m-auto max-w-[640px] w-full h-[45px] bg-nav-background-light dark:bg-nav-background-dark-light rounded-full flex justify-between items-center px-[6px]')}>
                                     {navItems.map((item) => (
-                                        <NavLink key={item.name} to={item.path} className={({ isActive }) => cn(`hover:scale-[0.9] hover:shadow-[0_1px_3px_rgba(0,0,0,0.4),_0_1px_2px_rgba(0,0,0,0.06)] rounded-full px-4 py-1 flex justify-center items-center gap-1 transition-all duration-150 ${isActive ? "bg-black text-white" : "bg-transparent text-black"}`)}>
-                                            {item.icon}
+                                        <NavLink key={item.name} to={item.path} className={({ isActive }) => cn(`rounded-full px-4 py-1 text-text-300 dark:text-text-dark-100 flex justify-center items-center gap-1 transition-all duration-150 ${isActive ? "bg-black text-white" : "bg-transparent"}`)}>
                                             <p className={cn("cursor-pointer")}>{item.name}</p>
                                         </NavLink>
                                     ))}
@@ -60,7 +54,7 @@ const Header: React.FC = () => {
                     </>
                 ) : (
                     <div id="logo">
-                        <h1 onClick={() => navigate('/')} className={cn(' px-4 py-1 rounded-full font-bold text-white bg-black')}>S-BRAIN</h1>
+                        <h1 onClick={() => navigate('/')} className={cn('text-text-300 dark:text-text-dark-100 font-bold tracking-widest')}>S-BRAIN</h1>
                     </div>
                 )}
 
@@ -75,7 +69,7 @@ const Header: React.FC = () => {
                             </button>
                         ) : (
                             <>
-                                <button onClick={() => setLoginIsOpen(true)} className={cn('font-[500] text-black  cursor-pointer')}>
+                                <button onClick={() => setLoginIsOpen(true)} className={cn('font-[500] text-text-300 dark:text-text-dark-100 cursor-pointer')}>
                                     Login
                                 </button>
                                 <Modal isOpen={loginIsOpen} onClose={() => setLoginIsOpen(false)} onSubmit={() => setLoginIsOpen(false)}>
@@ -89,7 +83,7 @@ const Header: React.FC = () => {
 
             {/* Mobile Menu Panel */}
             <motion.nav
-                className={cn("lg:hidden fixed top-0 left-0 h-full w-80 bg-white z-50")}
+                className={cn("lg:hidden fixed top-0 left-0 h-full w-80 bg-header-background dark:bg-header-background-dark z-50")}
                 initial={{ x: '-100%' }}
                 animate={{ x: isOpen ? 0 : '-100%' }}
                 transition={{
@@ -97,12 +91,12 @@ const Header: React.FC = () => {
                     ease: "easeInOut"
                 }}
             >
-                <div className={cn("p-6")}>
+                <div className={cn("p-6 ")}>
                     <div className={cn("flex justify-between items-center mb-8")}>
-                        <h1 className={cn("text-black")}>S-BRAIN</h1>
-                        <h1 onClick={() => setIsOpen(false)} ><RxCross2 fontSize="35px" /></h1>
+                        <h1 className={cn("text-text-300 dark:text-text-dark-100")}>S-BRAIN</h1>
+                        <h1 onClick={() => setIsOpen(false)} ><RxCross2 fontSize="35px" className={cn('text-text-300 dark:text-text-dark-100 cursor-pointer')}/></h1>
                     </div>
-                    <div className={cn("border-t pt-6")}> </div>
+                    <div className={cn("border-t pt-6 text-text-300 dark:text-text-dark-100")}> </div>
                     {status && (
                         <div className={cn("mb-8")}>
                             <ul className={cn("space-y-4")}>
@@ -111,8 +105,7 @@ const Header: React.FC = () => {
                                         <NavLink
                                             to={item.path}
                                             onClick={() => setIsOpen(false)}
-                                            className={({ isActive }) => cn(`flex items-center gap-3 p-3 rounded-lg transition-all duration-150 ${isActive ? "bg-black text-white" : "hover:bg-gray-100"}`)} >
-                                            {item.icon}
+                                            className={({ isActive }) => cn(`text-text-300 dark:text-text-dark-300 flex items-center gap-3 p-3 rounded-lg transition-all duration-150 ${isActive ? "bg-black dark:bg-white text-white dark:text-black" : "hover:bg-gray-100"}`)} >
                                             <span>{item.name}</span>
                                         </NavLink>
                                     </li>
--- frontend/src/components/ui/ThemeToggle.tsx
+++ frontend/src/components/ui/ThemeToggle.tsx
@@ -50,19 +50,19 @@ const ThemeToggle: React.FC<ThemeToggleProps> = ({
     return (
         <button
             onClick={toggleTheme}
-            className={cn(`relative inline-flex h-8 w-16 items-center rounded-full bg-gray-200 dark:bg-gray-700 transition-colors duration-300 focus:outline-none ${className}`)}
+            className={cn(`relative inline-flex h-8 w-16 items-center rounded-full bg-theme-toggle-background-light dark:bg-theme-toggle-background-dark-light transition-colors duration-300 focus:outline-none ${className}`)}
         >
             {/* Toggle slider */}
-            <span className={cn(`inline-block h-6 w-6 transform rounded-full bg-white dark:bg-gray-300 shadow-lg transition-transform duration-300 ${isDark ? 'translate-x-9' : 'translate-x-1' }`)}
+            <span className={cn(`inline-block h-6 w-6 transform rounded-full bg-theme-toggle-icon-background dark:bg-theme-toggle-icon-background-dark shadow-lg transition-transform duration-300 ${isDark ? 'translate-x-9' : 'translate-x-1' }`)}
             >
                 {/* Icon inside the slider */}
                 <span className={cn("flex h-full w-full items-center justify-center")}>
                     {isDark ? (
                         // Moon icon
-                        <IoIosMoon className={cn("text-white")}/>
+                        <IoIosMoon className={cn("text-text-dark-200")}/>
                     ) : (
                         // Sun icon
-                        <MdSunny className={cn("text-white")}/>
+                        <MdSunny className={cn("text-text-200")}/>
                     )}
                 </span>
             </span>
